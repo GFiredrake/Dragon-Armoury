6cb2e4ebe36c4bbd2c7a56bec94f5143d8e69d11
Robin Windon <rwindon@hotmail.co.uk>
2015-03-25 16:34:16 +0000

    Initial Commit

---
+++ Dragon Armoury/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dragon_Armoury.Startup))]
+namespace Dragon_Armoury
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
